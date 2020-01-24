d02a07ad5bf987bd802b2afda8390478ceb66b9c
g4zeru <haru25haru02@gmail.com>
2020-01-24 18:43:05 +0900

    add components

---
+++ UITableViewCell/___FILEBASENAME___.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___, Reusable, NibType {
+    typealias Dependency = <#Dependency#>
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func inject(_ dependency: <#Dependency#>) {
+        // Configure the view for the generated state
+    }
+}
