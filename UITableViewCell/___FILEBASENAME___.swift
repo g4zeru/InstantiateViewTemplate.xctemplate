a67228bc788fe643aa53902e914e20f3aaf7e158
g4zeru <haru25haru02@gmail.com>
2020-02-14 22:06:18 +0900

    fix templateinfo.plist

--- UITableViewCell/___FILEBASENAME___.swift
+++ UITableViewCell/___FILEBASENAME___.swift
@@ -3,7 +3,7 @@
 import UIKit
 import Instantiate
 
-class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___, Reusable, NibType {
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_viewClass___, Reusable, NibType {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
